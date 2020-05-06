caa4ac2d887497ad4a7a40c0553f57cb58e7b3e7
unknown <abdurrakib961@gmail.com>
2020-05-06 11:37:00 +0600

    Updates

---
+++ precache-manifest.4cb2fc79333507bca931afdc5aaf929f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b7ce8e566093687b9a9fae09d859e91",
+    "url": "/react-contact-manager/index.html"
+  },
+  {
+    "revision": "7ccede0a9a611275b5c5",
+    "url": "/react-contact-manager/static/css/main.e49e8e0c.chunk.css"
+  },
+  {
+    "revision": "95c9b35cd0b106317849",
+    "url": "/react-contact-manager/static/js/2.40bfdf49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-contact-manager/static/js/2.40bfdf49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ccede0a9a611275b5c5",
+    "url": "/react-contact-manager/static/js/main.c966a09f.chunk.js"
+  },
+  {
+    "revision": "f91e1ae1b7987184080d",
+    "url": "/react-contact-manager/static/js/runtime-main.a36a380e.js"
+  }
+]);
